80fb03e4cc2c907692d034bb2d68413621c523bf
Andrea <andrea.zorzi.94@gmail.com>
2017-05-15 08:12:58 +0200

    Add some docs

--- client/src/app/annotation/model/BaseAnnotation.ts
+++ client/src/app/annotation/model/BaseAnnotation.ts
@@ -12,9 +12,12 @@ export interface BaseAnnotation {
   timestamp?: Date
 }
 
+/**
+ * Values used to scale the annotation properly when the whole pdf page is scaled.
+ */
 export interface AnnotationScales {
-  origLeft: number,
-  origTop: number,
-  origScaleX: number,
-  origScaleY: number
+  origLeft: number, // Left coordinate of the annotation the first time it was drawn (Calculated respect to the pdf scale factor, see "origScaleX").
+  origTop: number, // Top coordinate of the annotation the first time it was drawn (Calculated respect to the pdf scale factor, see "origScaleY").
+  origScaleX: number, // Pdf scale factor (X axis) registered when the annotation has been drawn for the first time.
+  origScaleY: number // Pdf scale factor (Y axis) registered when the annotation has been drawn for the first time.
 }
